f73f650871f7fff656e9889ed3aebc488826485a
wwchang <wanw95@gmail.com>
2025-08-03 01:47:08 +0900

    테스트

--- dags/dags_Maple_API_ETL.py
+++ dags/dags_Maple_API_ETL.py
@@ -1,4 +1,17 @@
 from airflow import DAG
 from airflow.providers.standard.decorators.python import PythonOperator
+import pendulum
+
+with DAG(
+    dag_id ='dags_Maple_API_ETL',
+    schedule= None,
+    start_date=pendulum.datetime(2025,8,1,tz="Asia/Seoul"),
+    catchup=False
+) as dag:
+    
+    Test_Api = PythonOperator(
+        task_id='test_api'
+    )
 
 
+    Test_Api
