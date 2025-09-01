c5651450851cab207ca2e68d1523b6d69f663ffc
wwchang <wanw95@gmail.com>
2025-09-01 13:48:17 +0900

    API 호출시, 22023년12월21일 이후 데이터는 호출 불가에 따른 조건 추가

--- dags/dag_api_character_basic.py
+++ dags/dag_api_character_basic.py
@@ -13,9 +13,16 @@ with DAG(
     description="캐릭터 기본 정보 조회",
     catchup=False
 ) as dag:
+    '''
+    api 호출시, 2023년 12월21일 데이터부터 조회가 가능함. 단, 계정 정보 조회 API는 기간과 무관하게 조회가된다.
+    캐릭터 정보를 조회할 때, ocid를 파라미터로 입력해야하는데, 현재 len(ocid)가 32인 값과 64인 값 두 종류가 있다.
+    이때, 파라미터로 len(ocid) =64인 파라미터를 던지면, Invalid ID 오류가 발생한다.
+    이는 len(ocid)=32 인 캐릭터들이 데이터 조회 기준이 되는 2023년 12월21일 이후 생성된 캐릭터임을 짐작할 수 있다.
+    따라서 ocid_list에서 len(ocid)=64인 데이터는 대상에서 제외한다.
+    '''
     def ocid_list(**kwargs):
         hook = OdbcHook(odbc_conn_id='conn-db-mssql-maple',driver="ODBC Driver 18 for SQL Server")  #Airflow connection정보
-        sql = "SELECT ocid FROM character_list WHERE ocid NOT IN (SELECT ocid FROM character_basic );" #이 경우, 1회성에 그치게 되지만, API 호출 제한이 있으므로, 우선 ocid가 DB에 없는 경우만 불러오기 위함
+        sql = "SELECT ocid FROM character_list WHERE ocid NOT IN (SELECT ocid FROM character_basic ) AND LEN(ocid)=32;" #이 경우, 1회성에 그치게 되지만, API 호출 제한이 있으므로, 우선 ocid가 DB에 없는 경우만 불러오기 위함
         rows= hook.get_records(sql)
         
         return [r[0] for r in rows] #ocid 리스트 형태로 적재
