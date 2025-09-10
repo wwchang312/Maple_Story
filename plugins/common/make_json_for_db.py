2401fa7215688314568def35372704fe695a28a3
wwchang <wanw95@gmail.com>
2025-09-10 16:17:39 +0900

    table_list 주석처리

--- plugins/common/make_json_for_db.py
+++ plugins/common/make_json_for_db.py
@@ -1,13 +1,13 @@
 def make_json_for_db(obj,data_nm=None,ocid=None):
     import json
-    table_list = []
+    # table_list = []
     table_data = {}
     
     def make_dict_json(table_nm,lis):
-        nonlocal table_list,table_data
+        nonlocal table_data #,table_list
         
-        if table_nm not in table_list:
-            table_list.append(table_nm)
+        # if table_nm not in table_list:
+            # table_list.append(table_nm)
         if table_nm not in table_data:
             table_data[table_nm]=[]
         
@@ -43,18 +43,18 @@ def make_json_for_db(obj,data_nm=None,ocid=None):
     
     if single_row:
         target = data_nm or "main_table"
-        if target not in table_list:
-            table_list.append(target)
+        # if target not in table_list:
+            # table_list.append(target)
         table_data[target] = [single_row]
             
     
     for t in table_data:
         table_data[t] = json.dumps(table_data[t],ensure_ascii=False)
     
-    if data_nm and data_nm in table_list:
-        table_list = [data_nm] +[t for t in table_list if t != data_nm]
+    # if data_nm and data_nm in table_list:
+        # table_list = [data_nm] +[t for t in table_list if t != data_nm]
         
-    return table_list,table_data
+    return table_data #,table_list 
             
     
         
