98c7a25a860da6eeded7b89f43b44efe26784ef3
wwchang <wanw95@gmail.com>
2025-09-18 15:50:28 +0900

    함수 수정

--- plugins/common/make_json_for_db.py
+++ plugins/common/make_json_for_db.py
@@ -1,7 +1,6 @@
 
 def make_json_for_db(obj,data_nm=None,ocid=None,account_id=None):
     import json
-    
     table_data = {}
     
     def make_json_list(table_nm,lis):
@@ -46,7 +45,12 @@ def make_json_for_db(obj,data_nm=None,ocid=None,account_id=None):
     #상위 단일 row는 data_nm을 테이블 명으로
     
     if single_row:
-        target = data_nm or "main_table"
+        target = data_nm.replace('-','_') or "main_table"
+        
+        # 테이블 명으로 사용하는 data_nm과 list를 value로 사용하는 key값이 중복된 경우, data_nm에 _basic을 붙여서 이를 구분한다.
+        if table_data.get(target):
+            target=data_nm +"_common"
+        
         table_data[target] = [single_row]
             
     
@@ -56,6 +60,11 @@ def make_json_for_db(obj,data_nm=None,ocid=None,account_id=None):
     
     for t in table_data:
         table_data[t] = json.dumps(table_data[t],ensure_ascii=False)
+        
+    # single_row table이 나머지 테이블의 부모 테이블이 되므로, 먼저 돌기 위한 dic 순서 재조정
+    last_key, last_value = list(table_data.items())[-1]
+    table_data = {last_key: last_value, **{k: v for k, v in list(table_data.items())[:-1]}}
 
         
     return table_data
+            
