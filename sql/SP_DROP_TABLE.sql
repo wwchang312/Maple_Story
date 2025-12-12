db0cf70ff625d1d45535e758af6168139c45b652
wanw95 <wanw95@naver.com>
2025-12-12 16:26:28 +0900

    테이블 삭제 프로시저 추가

--- sql/SP_DROP_TABLE.sql
+++ sql/SP_DROP_TABLE.sql
@@ -1,44 +1,89 @@
-CREATE OR ALTER PROCEDURE dbo.SP_DROP_TABLE
-	@schema_nm SYSNAME = 'dbo' ,
-	@table_nm	NVARCHAR(128)
+CREATE OR ALTER PROCEDURE maple.SP_DROP_TABLE
+(
+	@schema_nm 		NVARCHAR(64) = 'maple',
+	@table_type 	INT
+) 
 AS
 BEGIN
 	SET NOCOUNT ON;
 
-	DECLARE @sql NVARCHAR(MAX) = N'';
+
+	/*
+	 table_type값을 입력받아 각 테이블 목적에 따른 삭제 구현
+	 0 - 전체
+	 1 - 이력 테이블 삭제
+	 2 - 뷰 삭제
+	 
+	 본 프로시저는 테이블 구조 전체를 다시 설계할때 사용하기 위함이므로
+	 특정 테이블을 지정하여 삭제하는 경우는 고려하지 않는다. 
+	 
+	 본테이블을 삭제하는 경우, 이와 관련된 뷰와 이력 테이블도 같이 삭제되야한다고 판단하여, 이력 테이블 삭제와 뷰 테이블 삭제만 별도로 분리함.
+	 */
+
+	DECLARE @sql	NVARCHAR(MAX);
 	
-	--1. table_nm 변수에 ALL 입력시 전체 테이블 삭제
-	
-	IF @table_nm = 'ALL'
+	-- 테이블 전체 삭제
+	IF @table_type = 0
 	BEGIN
 		
-		SELECT @SQL = STRING_AGG('DROP TABLE ' + QUOTENAME(@schema_nm) + '.' +QUOTENAME(t.name),';')
-			FROM sys.tables t
-			INNER JOIN sys.schemas s 
-				ON t.schema_id=s.schema_id
-			WHERE s.name = @schema_nm;	
-	
-	END
-	ELSE
-	BEGIN
-	--2. table_nm에 특정 테이블 명 입력시, 해당 테이블만 삭제
+		-- 제약 조건이 있는 자식 테이블 우선 삭제
+				
+		SELECT @sql=STRING_AGG(CONCAT('DROP TABLE ',s.name,'.',t.name),';')
+		FROM sys.foreign_keys fk 
+		JOIN sys.tables t ON fk.parent_object_id =t.object_id 
+		JOIN sys.schemas s ON s.schema_id=t.schema_id
+		WHERE s.name=@schema_nm;
 		
-		SET @sql = 'DROP TABLE' + QUOTENAME(@schema_nm) + '.' + QUOTENAME(@table_nm);
-	END
+		EXEC sp_executesql @sql ;
+		
+		-- 나머지 부모 테이블 삭제
+		SELECT @sql = STRING_AGG(CONCAT('DROP TABLE ',@schema_nm,'.',TABLE_NAME),';')
+		FROM INFORMATION_SCHEMA.TABLES t 
+		WHERE TABLE_SCHEMA =@schema_nm AND TABLE_TYPE='BASE TABLE';
+		
+		EXEC sp_executesql @sql ;
 	
-	-- sql 변수가 비어있지 않을 때만 실행
-	
-	IF @sql <> ''
-	BEGIN
+		-- 뷰 삭제
+		SELECT @sql = STRING_AGG(CONCAT('DROP VIEW ',@schema_nm,'.',TABLE_NAME),';')
+		FROM INFORMATION_SCHEMA.TABLES t 
+		WHERE TABLE_SCHEMA =@schema_nm AND TABLE_TYPE='VIEW';
+		
 		EXEC sp_executesql @sql;
-	END
-	ELSE
-	BEGIN
-		PRINT '대상이 되는 테이블이 없습니다.'
+		
+	
+		
 	END
 	
-END;
-GO
+	-- 이력 테이블 삭제
+	IF @table_type = 1
+	BEGIN
+	
+	SELECT @sql = STRING_AGG(CONCAT('DROP TABLE ',@schema_nm,'.',TABLE_NAME),';')
+		FROM INFORMATION_SCHEMA.TABLES t 
+		WHERE TABLE_SCHEMA =@schema_nm AND TABLE_TYPE='BASE TABLE' AND TABLE_NAME LIKE '%_hist%';
+		
+		EXEC sp_executesql @sql ;
+	
+	END
+	
+	-- View 삭제
+	IF @table_type =2
+	BEGIN
+		SELECT @sql = STRING_AGG(CONCAT('DROP VIEW ',@schema_nm,'.',TABLE_NAME),';')
+		FROM INFORMATION_SCHEMA.TABLES t 
+		WHERE TABLE_SCHEMA =@schema_nm AND TABLE_TYPE='VIEW';
+		
+		EXEC sp_executesql @sql;
+		
+	END
+	
+	
 	
 	
 END
+
+
+
+
+
+
