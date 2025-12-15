5ada03850a3ab2252f4e0ecec701f4e4e7e42713
wwchang <wanw95@gmail.com>
2025-12-15 16:50:40 +0900

    오류 수정

--- plugins/operators/maple_api_operator.py
+++ plugins/operators/maple_api_operator.py
@@ -69,7 +69,7 @@ class MapleApiOperator(BaseOperator):
         return contents
 
     ## json 문자열 dumping
-    def json_dumping(contents:dict , test):
+    def json_dumping(self,contents:dict):
 
         empty_list=[]
 
