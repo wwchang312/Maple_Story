8b1e006e54a450899e95570c1cbe455025d8e21d
wwchang <wanw95@gmail.com>
2025-12-17 16:21:26 +0900

    테스트

--- dags/DAG_Maple_Character_Basic_API.py
+++ dags/DAG_Maple_Character_Basic_API.py
@@ -44,9 +44,7 @@ with DAG(
     Maple_Character_Basic_ETL_task = MapleApiOperator.partial(
         task_id='Maple_Character_Basic_ETL_Task',
         data_nm='character/basic',
-        date =""" 
-            {% if ds ==  macros.datetime.strftime(macros.datetime.now(), '%Y%m%d') %} {{ none }} {% else %} {{ds}} {% endif %}
-              """
+        date ='{{ds}}'
         ).expand(
             ocid=generate_param_task.output,
             )
