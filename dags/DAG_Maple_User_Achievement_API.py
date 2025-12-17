c291bde88e9e9b15c7563d21dcd5a53faa643e10
wwchang <wanw95@gmail.com>
2025-12-17 12:18:15 +0900

    유저 업적 정보 DAG 이름 수정

---
+++ dags/DAG_Maple_User_Achievement_API.py
@@ -0,0 +1,21 @@
+from airflow import DAG
+from plugins.operators.Maple_API_Operator import MapleApiOperator
+import pendulum
+
+with DAG(
+    dag_id ='DAG_Maple_User_Achievement_API',
+    schedule= None,
+    start_date=pendulum.datetime(2025,8,1,tz="Asia/Seoul"),
+    tags= ['Maple','User Achievement','업적'],
+    description="메이플스토리 업적 정보",
+    catchup=False
+) as dag:
+    
+    Maple_User_Achievement_ETL_Task = MapleApiOperator(
+        task_id='Maple_User_Achievement_ETL_Task',
+        data_nm='user/achievement'
+    )
+
+
+
+
