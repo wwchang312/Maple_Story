c18884acb2bc982a538f147471054b1e543f0437
wwchang <wanw95@gmail.com>
2025-12-17 15:06:11 +0900

    태그 변경

--- dags/DAG_Maple_Character_List_API.py
+++ dags/DAG_Maple_Character_List_API.py
@@ -6,7 +6,7 @@ with DAG(
     dag_id ='DAG_Maple_Character_List_API',
     schedule= None,
     start_date=pendulum.datetime(2025,8,1,tz="Asia/Seoul"),
-    tags= ['maple','character list'],
+    tags= ['Maple','Character List'],
     description="메이플스토리 계정별 캐릭터 목록 추출",
     catchup=False
 ) as dag:
