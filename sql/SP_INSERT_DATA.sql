c0c5c6397cef04b0b105baea127dbe1d3419f5e8
wwchang <wanw95@gmail.com>
2026-01-02 18:11:58 +0900

    merge문 추가

--- sql/SP_INSERT_DATA.sql
+++ sql/SP_INSERT_DATA.sql
@@ -48,48 +48,76 @@ BEGIN
 	
 	-- 4. JOIN 조건절 ON 생성
 	
-	-- KEY값 추출
-	DECLARE @KeyList TABLE (col SYSNAME);
-	DECLARE @up_col NVARCHAR(MAX);
-	
-	INSERT INTO @KeyList 
-				SELECT COLUMN_NAME 
-				FROM INFORMATION_SCHEMA.KEY_COLUMN_USAGE kcu 
-				WHERE kcu.TABLE_NAME =@table_nm and kcu.TABLE_SCHEMA =@schema_nm;
+	-- ON 조건 생성
+	DECLARE @on NVARCHAR(MAX);
+	 
+	SELECT @on = CONCAT('t.',COLUMN_NAME,' = s.',COLUMN_NAME) 
+	FROM INFORMATION_SCHEMA.KEY_COLUMN_USAGE kcu 
+	WHERE kcu.TABLE_NAME =@table_nm and kcu.TABLE_SCHEMA =@schema_nm;
 
 	
-	-- 테이블 컬럼 추출
-	DECLARE @col NVARCHAR(MAX);
+	-- 변경 대상이 되는 일반 컬럼 추출
+	DECLARE @up_col NVARCHAR(MAX);
 	
-	SELECT @col = STRING_AGG(COLUMN_NAME, ',')
+	SELECT @up_col=STRING_AGG(CONCAT('t.',COLUMN_NAME,' = ','s.',COLUMN_NAME),', ')
+	FROM INFORMATION_SCHEMA.COLUMNS c 
+	WHERE c.TABLE_SCHEMA = @schema_nm AND c.TABLE_NAME = @table_nm 
+	AND c.COLUMN_NAME not in (SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.KEY_COLUMN_USAGE WHERE c.TABLE_NAME   =  @table_nm );
+	
+	
+	-- 테이블 컬럼 추출
+	DECLARE @sur_col NVARCHAR(MAX);
+	DECLARE @tar_col NVARCHAR(MAX);
+	
+	SELECT @sur_col = STRING_AGG(CONCAT('s.',COLUMN_NAME), ',')
 	FROM INFORMATION_SCHEMA.COLUMNS c 
 	WHERE c.TABLE_SCHEMA = @schema_nm
   	AND c.TABLE_NAME   = @table_nm; 
 	
+	SELECT @tar_col = STRING_AGG(COLUMN_NAME, ',')
+	FROM INFORMATION_SCHEMA.COLUMNS c 
+	WHERE c.TABLE_SCHEMA = @schema_nm
+  	AND c.TABLE_NAME   = @table_nm; 
+	
+	
+	
 	-- 5. 이력 테이블 존재 여부에 따른 분기
 	
 	DECLARE @sql NVARCHAR(MAX) = N'';
+	
+
 	
 	-- 이력 테이블 존재 
 	IF OBJECT_ID(@schema_nm+'.'+ @table_nm +'_hist','U') IS NOT NULL
 	BEGIN
 		
 	-- 이력 테이블에 데이터 INSERT	
-		SET @sql = N'INSERT INTO ' +@schema_nm +N'.'+@table_nm +N'_hist ('+@col+')'+ @sur +N';';
+		SET @sql = N'INSERT INTO ' +@schema_nm +N'.'+@table_nm +N'_hist ('+@tar_col+')'+ @sur +N';';
 		
-		SET @sql += N'TRUNCATE TABLE '+ @schema_nm +'.'+@table_nm + ';';
+		EXEC sp_executesql @sql;
 		
-		SET @sql += N'INSERT INTO ' +@schema_nm +'.'+@table_nm +'('+@col+') '+ @sur +';';
+		
+		SET @sql = N'MERGE INTO '+@schema_nm+'.'+@table_nm + N' t USING  (' + @sur +' ) s ON ' + @on + 
+					 N' WHEN MATCHED THEN UPDATE SET ' + @up_col+
+					 N' WHEN NOT MATCHED THEN INSERT (' + @tar_col + N' ) VALUES (' +@sur_col+');' ;
+--		SET @sql += N'DELETE'+ @schema_nm +'.'+@table_nm + ';';
+		
+--		SET @sql += N'INSERT INTO ' +@schema_nm +'.'+@table_nm +'('+@col+') '+ @sur +';';
 
 	
 	END
 	ELSE
 	BEGIN
-		SET @sql = N'TRUNCATE TABLE '+ @schema_nm +'.'+@table_nm + ';';
+--		SET @sql = N'DELETE '+ @schema_nm +'.'+@table_nm + ';';
 		
-		SET @sql += N'INSERT INTO ' +@schema_nm +'.'+@table_nm +'('+@col+') '+ @sur +';';
+--		SET @sql += N'INSERT INTO ' +@schema_nm +'.'+@table_nm +'('+@col+') '+ @sur +';';
+		
+		SET @sql = N'MERGE INTO '+@schema_nm+'.'+@table_nm + N't USING  (' + @sur +' ) s ON ' + @on + 
+					 N' WHEN MATCHED THEN UPDATE SET ' + @up_col+
+					 N' WHEN NOT MATCHED THEN INSERT (' +@tar_col + N' ) VALUES (' +@sur_col+');' ;
 
 	END
+	
 	
 	
 	
