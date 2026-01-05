37aa320562a0500d6f9b8d87fc9da6c677998877
wwchang <wanw95@gmail.com>
2026-01-05 14:24:56 +0900

    Cron 스케줄 추가

--- dags/DAG_Maple_Character_List_API.py
+++ dags/DAG_Maple_Character_List_API.py
@@ -4,7 +4,7 @@ import pendulum
 
 with DAG(
     dag_id ='DAG_Maple_Character_List_API',
-    schedule= None,
+    schedule= '0 0 * * 4', #매주 목요일 수행
     start_date=pendulum.datetime(2025,8,1,tz="Asia/Seoul"),
     tags= ['Maple','Character List'],
     description="메이플스토리 계정별 캐릭터 목록 추출",
--- dags/DAG_Maple_User_Achievement_API.py
+++ dags/DAG_Maple_User_Achievement_API.py
@@ -4,7 +4,7 @@ import pendulum
 
 with DAG(
     dag_id ='DAG_Maple_User_Achievement_API',
-    schedule= None,
+    schedule= '0 0 * * 4', #매주 목요일 수행
     start_date=pendulum.datetime(2025,8,1,tz="Asia/Seoul"),
     tags= ['Maple','User Achievement','업적'],
     description="메이플스토리 업적 정보",
