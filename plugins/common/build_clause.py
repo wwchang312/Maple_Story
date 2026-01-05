3b35c4abd1c69a52b759525052a63f65d460200c
wwchang <wanw95@gmail.com>
2026-01-05 17:59:29 +0900

    IN 조건절 함수 추가

---
+++ plugins/common/build_clause.py
@@ -0,0 +1,21 @@
+'''
+DB에서 호출하고자하는 값이 복수개일 경우, 이를 IN절로 만들어 SQL문을 완성하기 위한 함수
+'''
+
+def normalize_value(v):
+    if v is None:
+        return []
+    if isinstance(v,str):
+        return[v]
+    return list(v)
+
+def build_in_clause(values):
+    vals=normalize_value(values)
+
+    if not vals:
+        return None , []
+    
+    placeholders = ",".join(["?"] * len(vals))
+    return f"IN ({placeholders})", vals
+
+
