f0a9cba3ff19643071e3f41b45c635337c08feea
wwchang <wanw95@gmail.com>
2026-01-15 11:31:06 +0900

    DAG Asset 확인

---
+++ dags/DAG_Maple_Character_Popularity_API,.py
@@ -0,0 +1,46 @@
+from airflow import DAG
+from operators.maple_api_operator import MapleApiOperator
+import pendulum
+from airflow.providers.odbc.hooks.odbc import OdbcHook
+from airflow.providers.standard.operators.python import PythonOperator
+from airflow.decorators import task
+from airflow.sdk import Variable, Asset,AssetAlias
+
+maple_character_dataset =AssetAlias('maple_character_dataset')
+
+with DAG(
+    dag_id ='DAG_Maple_Character_Popularity_API',
+    schedule= [maple_character_dataset],
+    start_date=pendulum.datetime(2025,12,1,tz="Asia/Seoul"),
+    tags= ['Maple','인기도','Popularity'],
+    description="캐릭터 인기도 정보",
+    catchup=False,
+    default_args={
+        'pool':'maple_pool' #개발 API의 경우 초당 최대 호출 수가 5건이기 때문에 slot이 5개인 pool을 별도로 지정하여 이용 
+    }
+) as dag:
+    @task(task_id='inlet_from_asset',
+          inlets=[maple_character_dataset])
+    def inlet_from_asset(**kwargs):
+        inlet_events = kwargs.get('inlet_events')
+        events=inlet_events[maple_character_dataset]
+        print(events)
+
+
+#    Maple_Popularity_ETL_task = MapleApiOperator.partial(
+#        task_id='Maple_Popularity_ETL_task',
+#        data_nm='character/popularity',
+#        date = Variable.get("maple_date"), #기준일인 date 파라미터는 Airflow Variable을 통해 관리 (타 DAG에도 동일한 값을 적용하기 위함)
+#        ocid =
+
+        
+
+
+
+
+
+
+
+
+
+
