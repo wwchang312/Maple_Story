9c75d1fddc08cb8b4d54f3607e7aeaf18034935d
wwchang <wanw95@gmail.com>
2026-02-23 21:50:17 +0900

    태스크 데코레이터 그룹화

--- dags/DAG_Maple_Character_Hyper_Stat_API.py
+++ dags/DAG_Maple_Character_Hyper_Stat_API.py
@@ -3,8 +3,9 @@ from operators.maple_api_operator import MapleApiOperator
 import pendulum
 from airflow.providers.odbc.hooks.odbc import OdbcHook
 from airflow.providers.standard.operators.python import PythonOperator
-from airflow.sdk import Variable, Asset
-from common.data_from_meta import data_from_meta
+from airflow.sdk import Variable, Asset,task_group,task
+
+# from common.data_from_meta import data_from_meta 태스크 데코레이터 공통화 함수화 로직 연구중
 
 maple_character_info = Asset('maple_character_info')
 
@@ -19,8 +20,33 @@ with DAG(
         'pool':'maple_pool' #개발 API의 경우 초당 최대 호출 수가 5건이기 때문에 slot이 5개인 pool을 별도로 지정하여 이용 
     }
 ) as dag:
-    
-    ocid, view_date = data_from_meta(Asset_inlet = 'maple_character_info')
+    @task_group(group_id ='get_meta')
+    def get_meta():
+        # get ocid data from meta
+        @task(task_id='ocid_from_mata',
+            inlets=[maple_character_info])
+        def ocid_from_meta(**kwargs):
+            inlet_events = kwargs.get('inlet_events')
+            events = inlet_events[Asset('maple_character_info')]
+            ocid = events[-1].extra['ocid']
+            return ocid
+        
+        # get view_date data from meta
+        @task(task_id = 'view_date_from_meta',
+            inlets=[maple_character_info])
+        def view_date_from_meta(**kwargs):
+            inlet_events = kwargs.get('inlet_events')
+            events = inlet_events[Asset('maple_character_info')]
+            view_date = events[-1].extra['view_date']
+            return view_date
+
+        ocid = ocid_from_meta()
+        view_date = view_date_from_meta()
+        
+        return ocid, view_date
+
+
+    ocid, view_date =get_meta()
 
     maple_hyper_stat_ETL_task = MapleApiOperator.partial(
         task_id='maple_hyper_stat_ETL_task',
