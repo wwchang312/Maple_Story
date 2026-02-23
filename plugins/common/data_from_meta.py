197144e3ab4c2512073be337106efb95bc64143a
wwchang <wanw95@gmail.com>
2026-02-23 20:51:41 +0900

    함수 탭 수정

--- plugins/common/data_from_meta.py
+++ plugins/common/data_from_meta.py
@@ -5,11 +5,11 @@ def data_from_meta(*, group_id = 'date_from_meta'):
     with TaskGroup(group_id=group_id) as meta:
         @task(task_id='ocid_from_mata',
                 inlets=[maple_character_info])
-            def ocid_from_meta(**kwargs):
-                inlet_events = kwargs.get('inlet_events')
-                events = inlet_events[Asset('maple_character_info')]
-                ocid = events[-1].extra['ocid']
-                return ocid
+        def ocid_from_meta(**kwargs):
+            inlet_events = kwargs.get('inlet_events')
+            events = inlet_events[Asset('maple_character_info')]
+            ocid = events[-1].extra['ocid']
+            return ocid
             
         @task(task_id = 'view_date_from_meta',
             inlets=[maple_character_info])
