f7250a1d6b1160867496c276ba30192d0d10053e
wwchang <wanw95@gmail.com>
2026-02-24 17:04:21 +0900

    캐릭터 적용 세트효과 API 추가

---
+++ dags/DAG_Maple_Character_Set_Effect_API.py
@@ -0,0 +1,38 @@
+from operators.maple_api_operator import MapleApiOperator
+import pendulum
+from airflow.sdk import Asset,DAG
+from common.data_from_meta import data_from_meta
+
+maple_character_info = Asset('maple_character_info')
+
+with DAG(
+    dag_id ='DAG_Maple_Character_Set_Effect_API',
+    schedule= [maple_character_info],
+    start_date=pendulum.datetime(2026,1,1,tz="Asia/Seoul"),
+    tags= ['Maple','Character Set Effect','캐릭터 적용 세트 효과'],
+    description="캐릭터가 적용받고 있는 세트 효과 정보 조회",
+    catchup=False,
+    default_args={
+        'pool':'maple_pool' #개발 API의 경우 초당 최대 호출 수가 5건이기 때문에 slot이 5개인 pool을 별도로 지정하여 이용 
+    }
+) as dag:
+
+    ocid, view_date = data_from_meta(Asset_inlet=maple_character_info,Asset_inlet_nm ='maple_character_info')
+
+    maple_character_set_effect_ETL_task = MapleApiOperator.partial(
+        task_id='maple_character_set_effect_ETL_task',
+        data_nm='character/set-effect').expand(
+            ocid=ocid,
+            date = view_date
+            )
+
+
+
+
+
+
+
+
+
+
+
