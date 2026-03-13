133919e1e6b213416927b5faec62426dcc2d5668
wwchang <wanw95@gmail.com>
2026-03-13 16:03:55 +0900

    데이터 외부 배출 오퍼레이터 추가

---
+++ plugins/operators/export_date_to_excel._operator.py
@@ -0,0 +1,23 @@
+from airflow.sdk.bases.operator import BaseOperator
+from airflow.providers.odbc.hooks.odbc import OdbcHook
+
+
+
+
+class MapleApiOperator(BaseOperator):
+
+    template_fields= ('schema_nm')
+
+    def __init__(self,schema_nm:str,**kwargs):
+
+        super().__init__(**kwargs)
+        self.schema_nm = schema_nm
+
+    def execute(self, context):
+
+        #Mssql Server connect
+        hook = OdbcHook(odbc_conn_id='conn-db-mssql-maple',driver="ODBC Driver 18 for SQL Server")  #Airflow connection정보
+        sql = "SELECT v.name FROM sys.schemas s INNER JOIN sys.views v ON s.schema_id = v.schema_id WHERE s.name= ? "
+        schema_nm = self.schema_nm
+        
+        print(hook.run(sql, parameters=(schema_nm)))
