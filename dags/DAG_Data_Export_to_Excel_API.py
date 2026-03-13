f92ffa602829be8f8c52b4d21d46abde28305e9b
wwchang <wanw95@gmail.com>
2026-03-13 20:34:33 +0900

    매일 자정 실행되도록 schedule 설정

--- dags/DAG_Data_Export_to_Excel_API.py
+++ dags/DAG_Data_Export_to_Excel_API.py
@@ -4,7 +4,7 @@ from airflow.sdk import DAG
 
 with DAG(
     dag_id= 'DAG_Data_Export_to_Excel_API',
-    schedule= None, #'0 0 * * *', #매일 자정 실행
+    schedule= '0 0 * * *', #매일 자정 실행
     start_date= pendulum.datetime(2026,3,1,tz="Asia/Seoul"),
     tags= ['Maple','Excel','Export'],
     description= "데이터 엑셀 및 외부 방출 DAG",
