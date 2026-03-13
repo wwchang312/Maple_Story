0f1ed42eba79ac5fb491762bb093782430905967
wwchang <wanw95@gmail.com>
2026-03-13 16:11:22 +0900

    엑셀 외부 방출 DAG 추가

---
+++ dags/DAG_Data_Export_to_Excel_API.py
@@ -0,0 +1,16 @@
+from operators.export_date_to_excel_operator import export_date_to_excel_operator
+import pendulum
+from airflow.sdk import DAG
+
+with DAG(
+    dag_id= 'DAG_Data_Export_to_Excel_API',
+    schedule= '0 0 * * *', #매일 자정 실행
+    start_date= pendulum.datetime(2026,3,1,tz="Asia/Seoul"),
+    tags= ['Maple','Excel','Exort'],
+    description= "데이터 엑셀 및 외부 방출 DAG",
+    catchup= False
+)as dag:
+    
+    exoprt_data_to_excel_task = export_date_to_excel_operator(
+        task_id ='exoprt_data_to_excel_task',
+    )
