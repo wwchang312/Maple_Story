2602c5e4d8f339d63d7c64c6dc959fe016862308
wwchang <wanw95@gmail.com>
2026-03-13 16:22:20 +0900

    파라미터 누락 수정

--- dags/DAG_Data_Export_to_Excel_API.py
+++ dags/DAG_Data_Export_to_Excel_API.py
@@ -1,4 +1,4 @@
-from operators.export_date_to_excel_operator import export_date_to_excel_operator
+from operators.export_data_to_excel_operator import export_data_to_excel_operator
 import pendulum
 from airflow.sdk import DAG
 
@@ -6,11 +6,12 @@ with DAG(
     dag_id= 'DAG_Data_Export_to_Excel_API',
     schedule= '0 0 * * *', #매일 자정 실행
     start_date= pendulum.datetime(2026,3,1,tz="Asia/Seoul"),
-    tags= ['Maple','Excel','Exort'],
+    tags= ['Maple','Excel','Export'],
     description= "데이터 엑셀 및 외부 방출 DAG",
     catchup= False
 )as dag:
     
-    exoprt_data_to_excel_task = export_date_to_excel_operator(
+    exoprt_data_to_excel_task = export_data_to_excel_operator(
         task_id ='exoprt_data_to_excel_task',
+        schema_nm='pub'
     )
