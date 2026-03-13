edb103f9cca36ae18355898e8afd91d5b123a3cd
wwchang <wanw95@gmail.com>
2026-03-13 16:29:21 +0900

    DAG 수행 schedule 임시 None 처리

--- dags/DAG_Data_Export_to_Excel_API.py
+++ dags/DAG_Data_Export_to_Excel_API.py
@@ -4,13 +4,13 @@ from airflow.sdk import DAG
 
 with DAG(
     dag_id= 'DAG_Data_Export_to_Excel_API',
-    schedule= '0 0 * * *', #매일 자정 실행
+    schedule= None, #'0 0 * * *', #매일 자정 실행
     start_date= pendulum.datetime(2026,3,1,tz="Asia/Seoul"),
     tags= ['Maple','Excel','Export'],
     description= "데이터 엑셀 및 외부 방출 DAG",
     catchup= False
 )as dag:
-    
+
     exoprt_data_to_excel_task = export_data_to_excel_operator(
         task_id ='exoprt_data_to_excel_task',
         schema_nm='pub'
